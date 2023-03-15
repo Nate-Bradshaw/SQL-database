cda005bd1f5176b95a61905c4a9a0ca8c5fbf260
Nayt <nate.bradshaw5959@gmail.com>
2023-03-15 16:56:10 +0000

    start of something

--- app.js
+++ app.js
@@ -1,17 +1,58 @@
 const http = require('http');
+const url = require('url');
+const sqlite3 = require('sqlite3');
 
 const hostname = '127.0.0.1';
-const port = 3000;
+const port = 8080;
 
-const numA = 2;
-const numB = 8;
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  let numC = numB / numA;
-  res.end('ostrich-copter is cool ' + numC);
+  if (req.method === 'GET') {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(`
+      <form method="POST">
+        <label for="input-field">Input Field:</label>
+        <input type="text" id="input-field" name="input-field">
+        <button type="submit">Submit</button>
+      </form>
+    `);
+    res.end();
+  } 
+  else if (req.method === 'POST') {
+    let body = '';
+    req.on('data', chunk => {
+      body += chunk.toString();
+      console.log(body);
+    });
+
+    const inputField = url.parse(`?${body}`, true).query['input-field'];
+    res.write(`<p>You entered: ${inputField}</p>`);
+
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(`
+      <form method="POST">
+        <label for="input-field">Input Field:</label>
+        <input type="text" id="input-field" name="input-field">
+        <button type="submit">Submit</button>
+      </form>
+    `);
+    res.end();
+  }
 });
+
+function testDB(){
+  const db = new sqlite3.Database('mydatabase.db');
+
+  db.run(`CREATE TABLE mytable (
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    age INTEGER
+  )`);
+
+  return db;
+
+  db.close();
+}
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
